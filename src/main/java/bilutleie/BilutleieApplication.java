84dcc7f81cd90d9c7fff94e88781d831fc9ff4f1
599031 <599031@stud.hvl.no>
2023-02-10 11:45:11 +0100

    Initial commit

---
+++ src/main/java/bilutleie/BilutleieApplication.java
@@ -0,0 +1,13 @@
+package bilutleie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilutleieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilutleieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bilutleie/BilutleieApplicationTests.java
@@ -0,0 +1,13 @@
+package bilutleie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilutleieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
